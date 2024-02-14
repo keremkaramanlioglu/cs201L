a8fc00ba5530e719ff59811ff609e7415159eb1b
kk034505 <kerem.karamanlioglu@ozu.edu.tr>
2024-02-14 15:44:06 +0300

    isPrime

--- Main.java
+++ Main.java
@@ -1,6 +1,41 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        File input = new File("input.txt");
+        isPrime(input);
+    }
+
+    static void isPrime(File inputFile) throws IOException {
+        Scanner reader = new Scanner(inputFile);
+        try (FileWriter writer = new FileWriter("out.txt")) {
+            String prime = "prime";
+            String not = "not";
+            while (reader.hasNextLine()) {
+                String num = reader.nextLine();
+                if (isPrimeNumber(Integer.parseInt(num))) {
+                    writer.append(prime + "\n");
+                } else {
+                    writer.append(not + "\n");
+                }
+            }
+        }
+    }
+      static boolean isPrimeNumber(int number) {
+        if(number <= 1) {
+            return false;
+        } else if(number == 2) {
+            return true;
+        } else {
+            for(int div = 2; div <= Math.sqrt(number) + 1; div++) {
+                if(number % div == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
